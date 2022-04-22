1ffa4e1c29bb7d5c439b11c487bb85b42cda546e
Andre Alves <andremralves@gmail.com>
2022-04-22 10:34:11 -0300

    add account app to installed_apps

--- crm/settings.py
+++ crm/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # MY APPS
+    'accounts.apps.AccountsConfig',
+    ####
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

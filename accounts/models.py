4a6313d95671e34a7b4d8903134fed1d69d91559
Andre Alves <andremralves@gmail.com>
2022-04-22 10:33:14 -0300

    fix parameter

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Lawyer', 'Lawyer'),
+        ('Doctor', 'Doctor')
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order():
+    customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                    null=True)
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL,
+                                   null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)

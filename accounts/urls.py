7d6c6f4b629b5f3c37d7b9ec7c403fe118ed089d
Andre Alves <andremralves@gmail.com>
2022-04-28 14:43:35 -0300

    new paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from . import views  # import functions from views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    path('cliente/<int:pk>', views.customer, name="customer"),
+    path('cliente/create', views.customer_create, name="customer_create"),
 ]

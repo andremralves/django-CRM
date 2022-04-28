0a1a02dd3e29b9fe4cf86363301faf3ddf984ccb
Andre Alves <andremralves@gmail.com>
2022-04-28 14:44:50 -0300

    new views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from accounts.models import Customer
+
 # Create your views here.
+
+
+def dashboard(request):
+    customers = Customer.objects.all()
+    print(customers)
+    context = {'customers': customers}
+    return render(request, 'accounts/dashboard.html', context)
+
+
+def customer(request, pk):
+    customers = Customer.objects.get(id=pk)
+    print(customers)
+    context = {'customer': customer}
+    return render(request, 'accounts/customer.html', context)
+
+
+def customer_create(request):
+    return render(request, 'accounts/customer_create.html')

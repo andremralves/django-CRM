bce524c07588378dd49dac32157d6631e27945ed
Andre Alves <andremralves@gmail.com>
2022-05-08 02:04:33 -0300

    moving templates to root directory

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views  # import functions from views
+from django.views.generic import TemplateView
 
-app_name = 'accounts'
+# when calling urls in templates views u need to put name space
+# app_name = "accounts"
 urlpatterns = [
-    path('', views.dashboard, name="dashboard"),
-    path('cliente/<int:pk>', views.customer, name="customer"),
-    path('cliente/create', views.customer_create, name="customer_create"),
+    path("", views.dashboard, name="dashboard"),
+    path("cliente/<int:pk>", views.customer, name="customer"),
+    path("cliente/create", views.customer_create, name="customer_create"),
+    path("about/", TemplateView.as_view(template_name="customer_create.html")),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from accounts.models import Customer, Order, Service
+from django.contrib import messages
 
-from accounts.models import Customer
+# Auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+# Generic views
+from django.views.generic import CreateView
 
 # Create your views here.
 
+# Extends the Generic View
+class LoginView(CreateView):
+    template_name = "registration/login.html"
 
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+
+@login_required(login_url="login")
 def dashboard(request):
     customers = Customer.objects.all()
-    print(customers)
-    context = {'customers': customers}
-    return render(request, 'accounts/dashboard.html', context)
+    orders = Order.objects.all()[0:5]
+    total_customers = customers.count()
+    total_orders = orders.count()
+    context = {
+        "customers": customers,
+        "total_customers": total_customers,
+        "total_orders": total_orders,
+        "orders": orders,
+    }
+    return render(request, "dashboard.html", context)
 
 
+@login_required(login_url="login")
 def customer(request, pk):
-    customers = Customer.objects.get(id=pk)
-    print(customers)
-    context = {'customer': customer}
-    return render(request, 'accounts/customer.html', context)
+    customer = Customer.objects.get(id=pk)
+    print(customer)
+    context = {"customer": customer}
+    return render(request, "customer.html", context)
 
 
+@login_required(login_url="login")
 def customer_create(request):
-    return render(request, 'accounts/customer_create.html')
+    return render(request, "customer_create.html")
